55061ddd4c687d6e246e471890de43d65d2155ef
Rosalyn Rowe <rrowe404@gmail.com>
2020-06-22 21:55:45 -0400

    generate project & library

---
+++ projects/krossr/client/src/lib/client.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-client',
+  template: `
+    <p>
+      client works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ClientComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/krossr/client/src/lib/client.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { ClientComponent } from './client.component';
+
+
+
+@NgModule({
+  declarations: [ClientComponent],
+  imports: [
+  ],
+  exports: [ClientComponent]
+})
+export class ClientModule { }
