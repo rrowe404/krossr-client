2a30bf58bec26204fbaa8cc8807f533786822c88
Rosalyn Rowe <rrowe404@gmail.com>
2020-08-14 13:04:03 -0400

    more coverage

--- projects/krossr/client/src/lib/Tile/TileComponent.spec.ts
+++ projects/krossr/client/src/lib/Tile/TileComponent.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { TileComponent } from './TileComponent';
+import { TileState } from './TileState';
 
 describe('TileComponent', () => {
     let fixture: ComponentFixture<TileComponent>
@@ -11,10 +12,42 @@ describe('TileComponent', () => {
         }).compileComponents();
 
         fixture = TestBed.createComponent(TileComponent);
+        component = fixture.componentInstance;
         fixture.detectChanges();
     });
     
     it('should be created', () => {
         expect(fixture).toBeTruthy();
+    });
+
+    it('should have a working isPendingAndNotSelected fn', () => {
+        component.pending = true;
+        component.selected = false;
+        expect(component.isPendingAndNotSelected()).toBeTrue();
+    });
+
+    it('should have a working isNotPending fn', () => {
+        component.pending = false;
+        expect(component.isNotPending()).toBeTrue();
+    });
+
+    it('should have a working fill fn', () => {
+        component.fill(TileState.pending);
+        expect(component.pending).toBeTruthy();
+
+        component.fill(TileState.marked);
+        expect(component.marked).toBeTruthy();
+        expect(component.selected).toBeFalsy();
+        expect(component.pending).toBeFalsy();
+
+        component.fill(TileState.selected);
+        expect(component.selected).toBeTruthy();
+        expect(component.marked).toBeFalsy();
+        expect(component.pending).toBeFalsy();
+
+        component.fill(TileState.selected, true);
+        expect(component.selected).toBeFalsy();
+
+        component.fill('');
     });
 });
