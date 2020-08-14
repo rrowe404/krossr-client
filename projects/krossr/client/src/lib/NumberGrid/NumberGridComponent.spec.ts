3c42f899b22bff5069d391d064a10180b3e3c369
Rosalyn Rowe <rrowe404@gmail.com>
2020-08-14 12:00:44 -0400

    more

---
+++ projects/krossr/client/src/lib/NumberGrid/NumberGridComponent.spec.ts
@@ -0,0 +1,25 @@
+import { NumberGridComponent } from './NumberGridComponent';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BooleanMatrix } from '../Matrix/BooleanMatrix';
+
+describe('NumberGridComponent', () => {
+    let fixture: ComponentFixture<NumberGridComponent>;
+    let component: NumberGridComponent;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                NumberGridComponent
+            ]
+        });
+
+        fixture = TestBed.createComponent(NumberGridComponent);
+        component = fixture.componentInstance;
+        component.goalMatrix = new BooleanMatrix(2, 2);
+        fixture.detectChanges();
+    });
+
+    it('should be created', () => {
+        expect(fixture).toBeTruthy();
+    });
+});
