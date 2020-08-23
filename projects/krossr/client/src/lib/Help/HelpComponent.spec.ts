bc7754f342d0f5c0d0f8f9925144998fabb5b5d6
Rosalyn Rowe <rrowe404@gmail.com>
2020-08-23 12:46:23 -0400

    coverage

---
+++ projects/krossr/client/src/lib/Help/HelpComponent.spec.ts
@@ -0,0 +1,35 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { HelpComponent } from './HelpComponent';
+import { HelpModule } from './HelpModule';
+import { MatDialogRef } from '@angular/material/dialog';
+
+describe('HelpComponent', () => {
+    let fixture: ComponentFixture<HelpComponent>;
+    let component: HelpComponent;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                HelpModule
+            ],
+            providers: [
+                { provide: MatDialogRef, useValue: { close: () => {} } }
+            ]
+        });
+
+        fixture = TestBed.createComponent(HelpComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should be created', () => {
+        expect(fixture).toBeTruthy();
+    });
+
+    it('should close', () => {
+        let matDialogRef: MatDialogRef<HelpComponent> = TestBed.inject(MatDialogRef);
+        spyOn(matDialogRef, 'close');
+        component.close();
+        expect(matDialogRef.close).toHaveBeenCalled();
+    });
+});
--- projects/krossr/client/src/lib/LevelSelectFilter/LevelSelectFilterComponent.spec.ts
+++ projects/krossr/client/src/lib/LevelSelectFilter/LevelSelectFilterComponent.spec.ts
@@ -5,6 +5,7 @@ import { MockStateService } from 'src/test/MockStateService';
 import { StateService } from '@uirouter/core';
 import { LevelSelectFilterService } from './LevelSelectFilterService';
 import { MockLevelSelectFilterService } from 'src/test/MockLevelSelectFilterService';
+import { LevelListFilterOptions } from '@krossr/types';
 
 describe('LevelSelectFilterComponent', () => {
     let fixture: ComponentFixture<LevelSelectFilterComponent>;
@@ -38,5 +39,37 @@ describe('LevelSelectFilterComponent', () => {
                 resolve();
             });
         });
+    });
+
+    it('should update size', () => {
+        component.sizeMap = { '5x5': 25 };
+
+        spyOn(component.refilter, 'emit');
+        component.updateSize('5x5');
+        expect(component.refilter.emit).toHaveBeenCalledWith({ sizeRestriction: '25' } as LevelListFilterOptions);
+    });
+
+    it('should update sort by', () => {
+        component.sortByMap = { 'Date': 'date' };
+
+        spyOn(component.refilter, 'emit');
+        component.updateSortBy('Date');
+        expect(component.refilter.emit).toHaveBeenCalledWith({ sortBy: 'date' } as LevelListFilterOptions);
+    });
+
+    it('should update search text', () => {
+        spyOn(component.refilter, 'emit');
+
+        component.updateSearchText('hobbitses');
+        component.onChange();
+        expect(component.refilter.emit).toHaveBeenCalledWith({ searchText: 'hobbitses' } as LevelListFilterOptions);
+    });
+
+    it('should update sort direction', () => {
+        component.sortDirectionMap = { 'Descending': 'DESC' };
+
+        spyOn(component.refilter, 'emit');
+        component.updateSortDirection('Descending');
+        expect(component.refilter.emit).toHaveBeenCalledWith({ sortDirection: 'DESC' } as LevelListFilterOptions);
     });
 });
