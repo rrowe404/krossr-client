1d20e773da7845a67f212d13eda7549cdca6920f
Rosalyn Rowe <rrowe404@gmail.com>
2020-09-15 14:29:42 -0400

    update krossr-types + remove clientUrl code

--- projects/krossr/client/src/lib/Config/RouterConfig.ts
+++ projects/krossr/client/src/lib/Config/RouterConfig.ts
@@ -1,16 +1,10 @@
-import { RouterParams } from '@krossr/types';
 import { UIRouter } from '@uirouter/core';
 import { HomeRoutes } from '../Routing/RouteNames';
 
 export function uiRouterConfigFn(router: UIRouter) {
     let rules = router.urlService.rules;
     rules.initial((matchValue, url) => {
-        let params = url.search as RouterParams;
-        /**
-         * this is used to implement the catch-all route on the backend
-         * to simply redirect to the index page and let the client router handle it
-         */
-        return params.clientUrl || { state: HomeRoutes.home };
+        return { state: HomeRoutes.home };
     });
 
     rules.otherwise(() => ({ state: HomeRoutes.notFound }));
