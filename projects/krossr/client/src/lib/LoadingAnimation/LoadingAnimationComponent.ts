dbc147da69958be446d04b542ceedbaddad1a955
Rosalyn Rowe <rrowe404@gmail.com>
2020-10-08 17:14:47 -0400

    fix loading animations so they won't block the screen when the condition is false

--- projects/krossr/client/src/lib/LoadingAnimation/LoadingAnimationComponent.ts
+++ projects/krossr/client/src/lib/LoadingAnimation/LoadingAnimationComponent.ts
@@ -1,11 +1,28 @@
-import { Component, Input } from '@angular/core';
+import { Component, DoCheck, ElementRef, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
     selector: 'krossr-loading-animation',
     styleUrls: ['./LoadingAnimationStyles.less'],
     templateUrl: './LoadingAnimationView.html'
 })
-export class LoadingAnimationComponent {
+export class LoadingAnimationComponent implements DoCheck {
     @Input() public condition: boolean;
     @Input() public pulse = true;
+
+    constructor(
+        private elementRef: ElementRef,
+        private renderer: Renderer2
+    ) {
+        this.element = this.elementRef.nativeElement;
+    }
+
+    private element: HTMLElement;
+
+    ngDoCheck() {
+        if (this.condition) {
+            this.renderer.removeClass(this.element, 'hidden');
+        } else {
+            this.renderer.addClass(this.element, 'hidden');
+        }
+    }
 }
