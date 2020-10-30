764f1178bf3202ee425c8cd84eba4a88104d06a2
Rosalyn Rowe <rrowe404@gmail.com>
2020-10-30 15:32:46 -0400

    get the complete level preview filling in tiles... now to fit them correctly and disable editing

--- projects/krossr/client/src/lib/CompleteLevelPreview/CompleteLevelPreviewComponent.ts
+++ projects/krossr/client/src/lib/CompleteLevelPreview/CompleteLevelPreviewComponent.ts
@@ -1,11 +1,33 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { LevelListLevelViewModel } from '@krossr/types';
+import { GameMatrix } from '../GameMatrix/GameMatrix';
+import { GameSize } from '../GameSize/GameSize';
+import { ILevel } from '../Level/Level';
+import { LevelDecoder } from '../LevelDecoder/LevelDecoder';
+import { BooleanMatrix } from '../Matrix/BooleanMatrix';
 
 @Component({
     selector: 'krossr-complete-level-preview',
     styleUrls: ['../LevelPreview/LevelPreviewStyles.less'],
     templateUrl: 'CompleteLevelPreviewView.html'
 })
-export class CompleteLevelPreviewComponent {
-    @Input() public level: LevelListLevelViewModel;
+export class CompleteLevelPreviewComponent implements OnInit {
+    @Input() public level: ILevel;
+
+    public gameSize: GameSize;
+    public gameMatrix: GameMatrix;
+
+    constructor(
+        private levelDecoder: LevelDecoder
+    ) {
+    }
+
+    ngOnInit() {
+        this.level.decodedLayout = this.levelDecoder.decodeLayout(this.level.layout);
+        let layout = this.level.decodedLayout;
+        let matrix = new BooleanMatrix(layout.length, layout.length);
+        matrix.initializeWith(layout);
+        this.gameMatrix = new GameMatrix(matrix, true);
+        this.gameSize = { width: '100px', height: '100px' }; // todo fix magic;
+    }
 }
--- projects/krossr/client/src/lib/CompleteLevelPreview/CompleteLevelPreviewModule.ts
+++ projects/krossr/client/src/lib/CompleteLevelPreview/CompleteLevelPreviewModule.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
+import { GameModule } from '../Game/GameModule';
 import { CompleteLevelPreviewComponent } from './CompleteLevelPreviewComponent';
 
 @NgModule({
+    imports: [
+        GameModule
+    ],
     declarations: [ CompleteLevelPreviewComponent ],
     entryComponents: [ CompleteLevelPreviewComponent ],
     exports: [ CompleteLevelPreviewComponent ]
