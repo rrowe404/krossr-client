99301841cab2b29e4207221452af291b72dd0267
Rosalyn Rowe <rrowe404@gmail.com>
2025-04-12 17:25:05 +0900

    fix tests

--- src/test/MockLevelService.ts
+++ src/test/MockLevelService.ts
@@ -1,10 +1,10 @@
 import { LevelService } from '../Level/LevelService';
 import { Injectable } from '@angular/core';
-import { CreateLevelBodyViewModel } from '@krossr/api';
+import { CreateLevelBodyViewModel, LevelViewModel } from '@krossr/api';
 
 @Injectable()
 export class MockLevelService extends LevelService {
     createLevel(params: CreateLevelBodyViewModel) {
-        return Promise.resolve({ ...params, layout: '', size: 0 });
+        return ({ ...params, layout: '', size: 0 });
     }
 }
